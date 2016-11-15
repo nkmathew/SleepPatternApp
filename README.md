a01bd17bdf4d4bca2129726587ef78d45ad01a29
nkmathew <kipkoechmathew@gmail.com>
2016-11-16 00:10:16 +0300

    Script for building the release apk

---
+++ README.md
@@ -0,0 +1,8 @@
+#### Installing Genymotion
+1. Create an account on their website
+2. Install Oracle virtual box
+3. Get genymotion app and install
+4. login with user and pass
+5. click on +(Add) , install device
+6. down load virtual device you want , and then start emulator
+7. run react-native run-android , check emulator app should be running there.
---
+++ build-apk.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+## Generate signing keys
+# keytool -genkey -v -keystore my-release-key.keystore -alias my-key-alias -keyalg RSA -keysize 2048 -validity 10000
+
+## Generate the installable build
+react-native bundle --platform android --dev false --entry-file index.android.js \
+  --bundle-output android/app/src/main/assets/index.android.bundle \
+  --assets-dest android/app/src/main/res/
+
+## Generate the build using gradle
+cd android && ./gradlew assembleRelease
+
+## Upload the APK to your phone. The -r flag will replace the existing app (if it exists)
+adb install -r ./app/build/outputs/apk/app-release-unsigned.apk
