9d77018a32d7f9fea17094bcc8ee83278964d293
nkmathew <kipkoechmathew@gmail.com>
2016-11-25 17:36:34 +0300

    Add command for moving keystore file to gradle folder

--- build-apk.sh
+++ build-apk.sh
@@ -2,6 +2,7 @@
 
 ## Generate signing keys
 # keytool -genkey -v -keystore my-release-key.keystore -alias my-key-alias -keyalg RSA -keysize 2048 -validity 10000
+# mv my-release-key.keystore android/app
 
 ## Generate the installable build
 react-native bundle --platform android --dev false --entry-file index.android.js \
