508bb21e3b5811a45f2cb244e86f7f6a2f401b0d
nkmathew <kipkoechmathew@gmail.com>
2016-11-26 16:56:52 +0300

    Can add records to the mysqlite db on button click

---
+++ SleepData.js
@@ -0,0 +1,77 @@
+import SQLite from 'react-native-sqlite-storage';
+
+export default class SleepData {
+  constructor(dbName = "SleepPatternApp.db") {
+    this.dbName = dbName;
+    this.connection = SQLite.openDatabase(
+      dbName, "1.0", "Sleeping App Database", 200000,
+      this.openCB, this.errorCB
+    );
+
+    // Initialize sleeping data tabletable
+    this.initTables();
+  }
+
+  /**
+   * Create the database if not already existing
+   */
+  initTables() {
+    this.connection.transaction((tx) => {
+      tx.executeSql(`
+      CREATE TABLE IF NOT EXISTS sleeping_data (
+        id         INTEGER  PRIMARY KEY NOT NULL,
+        curr_date  DATETIME DEFAULT NULL,
+        sleep_time DATETIME DEFAULT NULL,
+        wake_time  DATETIME DEFAULT NULL
+      )`);
+    });
+  }
+
+  /**
+   * Prints all the records in the table as a string
+   */
+  getAllRecords() {
+    this.connection.transaction((transact) => {
+      transact.executeSql(`SELECT * FROM sleeping_data`, [], (_, results) => {
+        var totalRecords = results.rows.length;
+        console.log('Records found: ', totalRecords);
+        for (let i = 0; i < totalRecords; i++) {
+          let row = results.rows.item(i);
+          let record =
+            `${row.id}, ${row.curr_date}, ${row.sleep_time}, ${row.wake_time}`;
+          console.log(record);
+        }
+      });
+    });
+  }
+
+  /**
+   * Stop the sleeping timer and record the time
+   */
+  startSleeping() {
+    this.connection.transaction((transact) => {
+      transact.executeSql(`
+        INSERT INTO sleeping_data (curr_date, sleep_time, wake_time) VALUES
+        (CURRENT_DATE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+      `);
+    });
+  }
+
+  /**
+   * Start the timer and record the current time
+   */
+  stopSleeping() {
+  }
+
+  errorCB(err) {
+    console.log("SQL Error: " + err);
+  }
+
+  successCB() {
+    console.log("SQL executed fine");
+  }
+
+  openCB() {
+    console.log("Database OPENED");
+  }
+}
--- index.android.js
+++ index.android.js
@@ -13,6 +13,7 @@ import {
   StyleSheet,
   Text,
   View,
+  Alert,
   ScrollView
 } from 'react-native';
 
@@ -35,10 +36,7 @@ import {
   Fab
 } from 'native-base';
 
-// import HomePage from './homepage.js';
-// import AnalyticsPage from './analytics.js';
-// import AllWidgets from './all-widgets.js';
-// import CountDown from './react-native-countdown.js';
+import SleepData from './SleepData.js';
 
 const css = StyleSheet.create({
   baseText: {
@@ -51,52 +49,15 @@ const css = StyleSheet.create({
   },
 });
 
-import SQLite from 'react-native-sqlite-storage';
-
 export default class SleepPatternApp extends Component {
   constructor() {
     super();
-
-    var db = SQLite.openDatabase(
-      "test.db", "1.0", "Test Database", 200000,
-      this.openCB, this.errorCB
-    );
-    db.transaction((tx) => {
-      // Initialize table
-      tx.executeSql(`
-      CREATE TABLE IF NOT EXISTS sleeping_data (
-        id INTEGER PRIMARY KEY,
-        firstname text NOT NULL,
-        lastname text NOT NULL
-      )`);
-      // Insert values
-      tx.executeSql(`
-      INSERT INTO sleeping_data (id, firstname, lastname) VALUES
-      (12, 'Mathew', 'Kipkoech')
-      `);
-      tx.executeSql(`SELECT * FROM sleeping_data`, [], (tx, results) => {
-        console.log("Db:", tx);
-        // Get rows with Web SQL Database spec compliance.
-        var len = results.rows.length;
-        console.log('Values found: ', len);
-        for (let i = 0; i < len; i++) {
-          let row = results.rows.item(i);
-          console.log(`Employee name: ${row.firstname} ${row.lastname}`);
-        }
-      });
-    });
+    this.sleepData = new SleepData();
   }
 
-  errorCB(err) {
-    console.log("SQL Error: " + err);
-  }
-
-  successCB() {
-    console.log("SQL executed fine");
-  }
-
-  openCB() {
-    console.log("Database OPENED");
+  buttonPress() {
+    this.sleepData.startSleeping();
+    this.sleepData.getAllRecords();
   }
 
   render() {
@@ -108,9 +69,18 @@ export default class SleepPatternApp extends Component {
           </Button>
           <Title style={css.titleText}>Home</Title>
         </Header>
+
         <Content backgroundColor='#1F2B40'>
-          <Text style={css.baseText}>Some lovely text to boot</Text>
+          <Card backgroundColor='#71CCFF'>
+            <CardItem backgroundColor='#71CCFF'>
+              <Button rounded onPress={this.buttonPress.bind(this)}>
+                <Icon name='md-timer'/>
+                Start Sleeping
+              </Button>
+            </CardItem>
+          </Card>
         </Content>
+
         <Footer backgroundColor='#020C1E'>
           <FooterTab>
             <Button active>
